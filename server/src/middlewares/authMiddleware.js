0eaaae1e457dd52544be6f0bbf375fae1d6df8c0
yatinsingh2007 <yatin4591@gmail.com>
2025-12-01 10:41:53 +0530

    authmiddleware created

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { prisma } = require("../../prisma/dbConfig");
+const authMiddleware = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userData = await prisma.user.findUnique({
+        where : {
+            id : decoded.id
+        }
+    });
+    if(!userData){
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    req.user = userData;
+    next();
+  } catch (err) {
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { authMiddleware };
